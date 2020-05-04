e2a6f8abe9d3d6a322fe34e0e6ba1fa9537a807e
Dave Sudia <dsudia@gospotcheck.com>
2020-05-04 10:50:26 -0600

    what is the damn syntax?

--- site-modules/mounted_disk/manifests/init.pp
+++ site-modules/mounted_disk/manifests/init.pp
@@ -3,22 +3,22 @@ define mounted_disk (
   $disk_name = '',
   $disk_size = ''
 ){
-  lvm::physical_volume { "/dev/disks/by-id/${disk_id}":
+  physical_volume { "/dev/disks/by-id/${disk_id}":
     ensure => present,
   }
 
-  lvm::volume_group { "/dev/disks/by-id/${disk_id}":
+  volume_group { "/dev/disks/by-id/${disk_id}":
     ensure           => present,
     physical_volumes => "/dev/disks/by-id/${disk_id}",
   }
 
-  lvm::logical_volume { "/dev/disks/by-id/${disk_id}":
+  logical_volume { "/dev/disks/by-id/${disk_id}":
     ensure       => present,
     volume_group => $disk_name,
     size         => $disk_size,
   }
 
-  lvm::filesystem { "/dev/disks/by-id/${disk_id}":
+  filesystem { "/dev/disks/by-id/${disk_id}":
     ensure  => present,
     fs_type => 'ext4',
     options => '-m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard',
