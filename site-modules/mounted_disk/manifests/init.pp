27045f6160af8670c301734e3d84126b2a4c0137
Dave Sudia <dsudia@gospotcheck.com>
2020-05-04 11:02:50 -0600

    fix naming of disk

--- site-modules/mounted_disk/manifests/init.pp
+++ site-modules/mounted_disk/manifests/init.pp
@@ -10,27 +10,23 @@ define mounted_disk (
   volume_group { $disk_id:
     ensure           => present,
     physical_volumes => "/dev/disk/by-id/${disk_id}",
-    require          => PhyiscalVolume["/dev/disk/by-id/${disk_id}"],
   }
 
   logical_volume { $disk_id:
     ensure       => present,
     volume_group => $disk_name,
     size         => $disk_size,
-    require      => VolumeGroup[$disk_id],
   }
 
   filesystem { "/dev/disk/by-id/${disk_id}":
     ensure  => present,
     fs_type => 'ext4',
     options => '-m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard',
-    require => LogicalVolume[$disk_id],
   }
 
   mount {"/mnt/disk/${disk_name}":
-    ensure  => 'mounted',
-    device  => "/dev/disk/by-id/${disk_id}",
-    fstype  => 'ext4',
-    require => Filesystem["/dev/disk/by-id/${disk_id}"]
+    ensure => 'mounted',
+    device => "/dev/disk/by-id/${disk_id}",
+    fstype => 'ext4',
   }
 }
