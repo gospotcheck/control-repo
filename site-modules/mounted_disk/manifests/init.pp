0eb5e73a3d461f21e227fd403bc61bae41bbea32
Dave Sudia <dsudia@gospotcheck.com>
2020-05-04 10:42:47 -0600

    updates lvm module usage

--- site-modules/mounted_disk/manifests/init.pp
+++ site-modules/mounted_disk/manifests/init.pp
@@ -3,19 +3,34 @@ define mounted_disk (
   $disk_name = '',
   $disk_size = ''
 ){
-  lvm::volume { $disk_id:
+  lvm::physical_volume { "/dev/disks/by-id/${disk_id}":
+    ensure => present,
+  }
+
+  lvm::volume_group { "/dev/disks/by-id/${disk_id}":
+    ensure           => present,
+    physical_volumes => "/dev/disks/by-id/${disk_id}",
+    require          => Lvm::PhysicalVolume["/dev/disks/by-id/${disk_id}"]
+  }
+
+  lvm::logical_volume { "/dev/disks/by-id/${disk_id}":
+    ensure       => present,
+    volume_group => $disk_name,
+    size         => $disk_size,
+    require      => Lvm::VolumeGroup["/dev/disks/by-id/${disk_id}"]
+  }
+
+  lvm::filesystem { "/dev/disks/by-id/${disk_id}":
     ensure  => present,
-    vg      => $disk_id,
-    pv      => "/dev/disk/by-id/${disk_id}",
-    fstype  => 'ext4',
-    size    => $disk_size,
-    options => '-m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard'
+    fs_type => 'ext4',
+    options => '-m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard',
+    require => Lvm::LogicalVolume["/dev/disks/by-id/${disk_id}"]
   }
 
   mount {"/mnt/disks/${disk_name}":
     ensure  => 'mounted',
     device  => "/dev/disks/by-id/${disk_id}",
     fstype  => 'ext4',
-    require => Lvm::Volume[$disk_name]
+    require => Lvm::Filesystem["/dev/disks/by-id/${disk_id}"]
   }
 }
