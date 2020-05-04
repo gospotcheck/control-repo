fc30b16a2afc6cf93187565bad7a8b910084d247
Dave Sudia <dsudia@gospotcheck.com>
2020-05-04 10:46:08 -0600

    removes require statements

--- site-modules/mounted_disk/manifests/init.pp
+++ site-modules/mounted_disk/manifests/init.pp
@@ -10,27 +10,23 @@ define mounted_disk (
   lvm::volume_group { "/dev/disks/by-id/${disk_id}":
     ensure           => present,
     physical_volumes => "/dev/disks/by-id/${disk_id}",
-    require          => Lvm::PhysicalVolume["/dev/disks/by-id/${disk_id}"]
   }
 
   lvm::logical_volume { "/dev/disks/by-id/${disk_id}":
     ensure       => present,
     volume_group => $disk_name,
     size         => $disk_size,
-    require      => Lvm::VolumeGroup["/dev/disks/by-id/${disk_id}"]
   }
 
   lvm::filesystem { "/dev/disks/by-id/${disk_id}":
     ensure  => present,
     fs_type => 'ext4',
     options => '-m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard',
-    require => Lvm::LogicalVolume["/dev/disks/by-id/${disk_id}"]
   }
 
   mount {"/mnt/disks/${disk_name}":
-    ensure  => 'mounted',
-    device  => "/dev/disks/by-id/${disk_id}",
-    fstype  => 'ext4',
-    require => Lvm::Filesystem["/dev/disks/by-id/${disk_id}"]
+    ensure => 'mounted',
+    device => "/dev/disks/by-id/${disk_id}",
+    fstype => 'ext4',
   }
 }
